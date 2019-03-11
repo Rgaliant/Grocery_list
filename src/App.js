3525c9fddcb480950cbb990180a33eb6aa7b209a
Ryan Gale <ryanwfgale@gmail.com>
2019-03-11 15:50:59 -0600

    add form and strike

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './List';
+import GroceryForm from './GroceryForm';
+
 
 class App extends Component {
+  state = {
+    groceries: [
+      { id: 1, name: "Milk", complete: true, },
+      { id: 2, name: "Cereal", complete: false, },
+      { id: 3, name: "Cheese", complete: false, },
+    ]
+  }
+  
+  getUniqId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+   }
+
+   addItem = (name) => {
+    const { groceries } = this.state;
+    const grocery = { id: this.getUniqId, name,  complete: false }
+    this.setState({ groceries: [grocery, ...groceries] }); 
+    }
+
+    handleClick = (id) => {
+      const { groceries } = this.state
+      this.setState({
+        groceries: this.state.groceries.map( grocery => {
+          if (grocery.id === id) {
+            return { ...grocery, complete: !grocery.complete,}
+            } 
+            return grocery;
+          })
+        })
+      }
+
+  renderGroceries = () => {
+    const { groceries, } = this.state;
+    return groceries.map( grocery => 
+      <li key={grocery.id}>{grocery.name}</li>
+    )
+  }
+
+
   render() {
+    const { groceries } = this.state;
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Grocery List</h1>
+        <ul>
+          <List name="Grocery List" items={this.state.groceries} groceryClick={this.handleClick} />
+          <GroceryForm addItem={this.addItem} />
+
+        </ul>
       </div>
     );
   }
